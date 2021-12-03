c68aa35b56a092d0f908f0921bd2519218e73b19
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-03 10:29:53 -0800

    working on test

--- index.js
+++ index.js
@@ -75,16 +75,22 @@ export const getFirstTwoArgs = (a, b, ...rest) => {
 //    return a NEW object, do not modify the object passed in to the function
 //    use spread operator to create a new object
 
-export const addSneakerCount = () => {};
+export const addSneakerCount = ({ shoes, ...rest }) => {
+  return { ...rest, shoes, sneakerCount: shoes.length };
+};
 
 // INPUT: brands from data.js
 // OUTPUT: the brand names listed
 // REQS: use Object.keys to solve
-export const getBrandNames = () => {};
+export const getBrandNames = (data) => {
+  return Object.keys(data);
+};
 
 // INPUT: brands from data.js
 // OUTPUT: total number of sneaker types across all brands (14)
-export const totalSneakerCount = () => {};
+export const totalSneakerCount = (data) => {
+  return Object.keys(data).reduce((acc, key) => (acc += data[key].shoes.length), 0);
+};
 
 // INPUT: An object
 // OUTPUT: An array with key value pairs converted to arrays
@@ -93,4 +99,6 @@ export const totalSneakerCount = () => {};
 // convertToArray({}) => []
 // Source: https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
 
-export const convertToArray = () => {};
+export const convertToArray = (obj) => {
+  return Object.keys(obj).map((key) => [key, obj]);
+};
